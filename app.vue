d415033bf59d90064de82043fdaa55fe9b6cc89e
Eyob Samuel <eysamuel@excellerentsolutions.com>
2023-04-10 16:38:16 +0300

    page is added

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <div>Hello World</div>
+    <NuxtLink to="/home">Home</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtPage />
+ 
   </div>
 </template>
---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const { data, refresh, pending } = await useLazyFetch('https://api.nuxtjs.dev/mountains')
+    function refreshMe(){
+        refresh()
+    }
+</script>
+
+<template>
+    <div>
+        <div v-if="!pending">Page visits: {{ data.length }}</div>
+        <div v-else>Loading...</div>
+        <button @click="refreshMe">refresh me</button>
+    </div>
+</template>
